6b754a27357b9562f73ffbd964bca26914dd4edf
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2022-05-04 10:13:33 -0400

    ENH: add '/api' prefix to 'console_output_update' endpoint

--- bluesky_queueserver_api/tests/common.py
+++ bluesky_queueserver_api/tests/common.py
@@ -4,7 +4,7 @@ from bluesky_queueserver.manager.tests.common import (  # noqa: F401
     set_qserver_zmq_address,
     set_qserver_zmq_public_key,
 )
-from bluesky_httpserver.server.tests.conftest import fastapi_server, fastapi_server_fs  # noqa: F401
+from bluesky_httpserver.tests.conftest import fastapi_server, fastapi_server_fs  # noqa: F401
 
 # from .common import re_manager, re_manager_pc_copy, re_manager_cmd, db_catalog  # noqa: F401
 
