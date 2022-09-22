29e9c2ff1c2210efce8ea404f8223e739d8a541c
Blaise Thompson <blaise@untzag.com>
2022-09-22 15:55:26 -0500

    black

--- bluesky_queueserver_api/_happi.py
+++ bluesky_queueserver_api/_happi.py
@@ -13,8 +13,7 @@ class REManagerAPIZMQItem(HappiItem):
     zmq_control_addr = EntryInfo("ZMQ control address.", enforce=str, optional=False)
     zmq_info_addr = EntryInfo("ZMQ info address.", enforce=str, optional=False)
     kwargs = copy.copy(HappiItem.kwargs)
-    kwargs.default = {"zmq_control_addr": "{{zmq_control_addr}}",
-                      "zmq_info_addr": "{{zmq_info_addr}}"}
+    kwargs.default = {"zmq_control_addr": "{{zmq_control_addr}}", "zmq_info_addr": "{{zmq_info_addr}}"}
     device_class = EntryInfo(default="bluesky_queueserver_api.zmq.REManagerAPI")
 
 
@@ -22,7 +21,5 @@ class REManagerAPIHTTPItem(HappiItem):
     http_server_uri = EntryInfo("HTTP server URI.", enforce=str, optional=False)
     http_auth_provider = EntryInfo("HTTP auth provider.", enforce=str, optional=False)
     kwargs = copy.copy(HappiItem.kwargs)
-    kwargs.default = {"http_server_uri": "{{http_server_uri}}",
-                      "http_auth_provider": "{{http_auth_provider}}"}
+    kwargs.default = {"http_server_uri": "{{http_server_uri}}", "http_auth_provider": "{{http_auth_provider}}"}
     device_class = EntryInfo(default="bluesky_queueserver_api.http.REManagerAPI")
-
